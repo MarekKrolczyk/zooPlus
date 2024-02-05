8c045ec5e2ea51e4e35be221e86904dbca83f664
Marek <marek.krolczyk@maandag.pl>
2024-02-05 12:04:12 +0100

    cart scenario comments update

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,28 +1,31 @@
-import test, { expect } from "../fixtures/basePages";
+import test, { expect } from "../fixtures/page-object.fixture";
 import items from "../items/items.json";
 
-test.beforeEach(async ({ page, cookiesPage }) => {
-  await page.goto('/');
+test.beforeEach(async ({ cookiesPage, cartPage }) => {
+  // Navigate to the cart page and accept cookies before each test
+  await cartPage.goto();
   await cookiesPage.acceptCookies();
 });
 
-test.describe("User can find, view details, add to cart, and verify prices in the cart", () => {
-  test("Should successfully find, view details, add to cart, and verify prices in the cart for a specific item", async ({ navbarPage, searchListPage, cartPage }) => {
+test.describe("Shopping Cart Functionality Tests", () => {
+  test("User can find, view details, add to cart, and verify prices in the cart for a specific item", async ({ navbarPage, searchListPage, cartPage }) => {
     // User finds the item using the search bar
     await navbarPage.findItem(items.DinoToy.name);
+    // Verify that the item is visible in the search list
     await expect(searchListPage.itemName(items.DinoToy.name)).toBeVisible();
 
     // User views details of the item
     await expect(searchListPage.priceOfItem(items.DinoToy.name, items.DinoToy.price)).toBeVisible();
 
     // User adds the item to the cart from the search list
-    await searchListPage.addThisItemToCard(items.DinoToy.name);
+    await searchListPage.addThisItemToCart(items.DinoToy.name);
 
-    // User clicks on the cart button
+    // User clicks on the cart button in the navigation bar
     await navbarPage.clickCartButton();
 
     // User verifies the item in the cart
     await expect(cartPage.productName(items.DinoToy.name)).toBeVisible();
+    // Verify the total price of the item in the cart, including shipment
     await expect(cartPage.productTotalPrice(items.DinoToy.priceWithShipment)).toBeVisible();
   });
 });
