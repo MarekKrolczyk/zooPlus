245f01acff87ee17016647bc12d958129cf4b06b
Marek <marek.krolczyk@maandag.pl>
2024-02-05 12:03:15 +0100

    pages reorganization

--- pages/navbar.page.ts
+++ pages/navbar.page.ts
@@ -1,20 +1,20 @@
-import { Page, Locator } from "@playwright/test";
+import { Page } from "@playwright/test";
+import { BasePage } from "./base.page";
 
-export default class NavbarPage {
-    readonly searchBar: Locator;
-    readonly searchButton: Locator;
-    readonly cartButton: Locator;
+export class NavbarPage extends BasePage {
+  searchBar = this.page.locator('[data-zta="search_form_input_desktop"]');
+  searchButton = this.page.locator('[data-zta="search_form_button_desktop"]');
+  cartButton = this.page.locator('[data-testid="MiniCartLink"]');
 
-    constructor(page: Page) {
-        this.searchBar = page.locator('[data-zta="search_form_input_desktop"]');
-        this.searchButton = page.locator('[data-zta="search_form_button_desktop"]');
-        this.cartButton = page.locator('[data-testid="MiniCartLink"]');
-    }
-    async findItem(itemName: string) {
-        await this.searchBar.fill(itemName);
-        await this.searchButton.click();
-    };
-    async clickCartButton() {
-        await this.cartButton.click();
-    };
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async findItem(itemName: string) {
+    await this.searchBar.fill(itemName);
+    await this.searchButton.click();
+  }
+  async clickCartButton() {
+    await this.cartButton.click();
+  }
 }
--- pages/searchList.page.ts
+++ pages/searchList.page.ts
@@ -1,21 +1,18 @@
-import { Page, Locator } from "@playwright/test";
+import { Page } from "@playwright/test";
+import { BasePage } from "./base.page"
 
-export default class SearchList {
+export  class SearchListPage extends BasePage {
     addItemToCartButton(itemName: string) { return this.page.locator(`(//div[@data-zta="product-box-list" and .//*[contains(text(), '${itemName}')]])//*[contains(@data-zta, 'add-to-cart') and @data-zta="add-to-cart"]`) }
     itemName(itemName: string) { return this.page.locator(`(//div[@data-zta="product-box-list" and .//*[contains(text(), '${itemName}')]])//*[@data-zta="product-link"]`) }
     priceOfItem(itemName: string, itemPrice: string) { return this.page.locator(`(//div[@data-zta="product-box-list" and .//*[contains(text(), '${itemName}')]]//*[@data-zta="product-link"]/ancestor::div[@data-zta="product-box-list"]//*[@data-zta="productStandardPriceAmount" and contains(text(), '${itemPrice}')])`) }
+    
+    firstListItem = this.page.locator('(//*[@data-zta="product-link"])[1]');
+    firstItemPrice = this.page.locator('(//*[@data-zta="productStandardPriceAmount"])[1]');
 
-    private page: Page;
-    readonly firstListItem: Locator;
-    readonly firstItemPrice: Locator;
-
-    constructor(page: Page) {
-        this.page = page;
-        this.firstListItem = page.locator('(//*[@data-zta="product-link"])[1]');
-        this.firstItemPrice = page.locator('(//*[@data-zta="productStandardPriceAmount"])[1]');
-        this.firstItemPrice = page.locator('(//*[@data-zta="productStandardPriceAmount"])[1]');
-    }
-    async addThisItemToCard(itemName: string) {
+  constructor(page: Page) {
+    super(page);
+  }
+    async addThisItemToCart(itemName: string) {
         await this.addItemToCartButton(itemName).click();
     };
 }
